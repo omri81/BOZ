892b7ad7108177cf0db872dea5de62f285fd6200
zoharIOS <zohar.ios.dev@gmail.com>
2018-02-21 23:36:04 +0000

    gif , button animation , constraints update

--- BOZ/controllers/MainViewController.swift
+++ BOZ/controllers/MainViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class MainViewController: BaseViewController {
 
+    @IBOutlet weak var quickDonationBtn: UIButton!
+    @IBOutlet weak var logoGifImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        logoGifImageView.loadGif(name: "logo")
+        quickDonationBtn.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        UIView.animate(withDuration: 2.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 6.0,
+                       options: .allowUserInteraction,
+                       animations: { [weak self] in
+                        self?.quickDonationBtn.transform = .identity
+            },
+                       completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
