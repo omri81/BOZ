be28f4976aa1793ce3db66e5497ad62fb88f3787
zoharIOS <zohar.ios.dev@gmail.com>
2018-02-21 20:22:47 +0000

    mainviewcontroller updated, baseviewontroller an mvc structure been made

---
+++ BOZ/controllers/basecontroller/BaseViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  BOZ
+//
+//  Created by MacMini on 20/02/2018.
+//  Copyright © 2018 Ovadia. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
