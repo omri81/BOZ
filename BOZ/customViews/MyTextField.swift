c5a4cec594b346d304a14d3f35b7daf6f5468f02
barbiv <bar_arb@icloud.com>
2018-03-02 16:36:25 +0200

    login screen added, still not functioning

---
+++ BOZ/customViews/MyTextField.swift
@@ -0,0 +1,36 @@
+
+//
+//  MyTextField.swift
+//  BOZ
+//
+//  Created by Bar Arbiv on 02/03/2018.
+//  Copyright © 2018 Ovadia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class MyTextField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.5)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
