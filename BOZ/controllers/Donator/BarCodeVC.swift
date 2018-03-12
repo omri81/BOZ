4e7bbc9312fc96ab3f2fc5ac7377a3b46532772d
zoharIOS <zohar.ios.dev@gmail.com>
2018-03-12 18:28:27 +0000

    start alamo fire in barcode

--- BOZ/controllers/Donator/BarCodeVC.swift
+++ BOZ/controllers/Donator/BarCodeVC.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class BarCodeVC: UIViewController {
-
+    var bookMark = ""
+    
+    @IBAction func barcode(_ sender: Any, forEvent event: UIEvent) {
+        getBarcode()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +26,29 @@ class BarCodeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getBarcode() {
+        let parameters: Parameters = [
+            "bookMark" : bookMark
+        ]
+        Alamofire.request(URL_PRODUCTS, method: HTTPMethod.post , parameters: parameters,
+                          encoding: JSONEncoding.default, headers: [:])
+            .validate(contentType: ["application/json"]).responseJSON { response in
+                switch response.result {
+                case .success:
+                    print("sucess response from server")
+                    guard let responseJSON = response.result.value as? [String: Any],
+                        let status = responseJSON["status"] as? String,
+                        let statusMsg = responseJSON["statusMsg"] as? String
+                        else {return}
+                    if status == "OK" {
+                        print("retrive success")
+                        guard let docs = responseJSON["docs"] as? [[String: Any]]
+                            else { return }
+                        print(docs)
+                        }
+                case .failure(let error):
+                        print("error:\(error)")
+                }
+        }
     }
-    */
-
 }
